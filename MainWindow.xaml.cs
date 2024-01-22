edd813e9e6f7e244d6bfc45c26dab886503a5c1c
RomanMykush <roman.mykush.dev@gmail.com>
2024-01-22 22:13:25 +0000

    Improved UI and added custom window title bar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
 namespace CryptoViewer
 {
@@ -19,6 +13,35 @@ namespace CryptoViewer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                WindowState = WindowState.Maximized;
+                return;
+            }
+            WindowState = WindowState.Normal;
         }
     }
 }
